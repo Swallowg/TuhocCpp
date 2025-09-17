1c3a1a1a87c60fe0eee19a4df34e5b82b779362f
NgocAnh <158980143+Swallowg@users.noreply.github.com>
2025-09-18 06:18:34 +0700

    HelloWorld

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/*Một chương trình C++ chỉ có duy nhất một hàm main.*/
+int main(){
+    //Đây là nơi chương trình C++ bắt đầu thực hiện các công việc
+
+    cout<<"Ôn lại C++ thôi nào"<<endl;
+
+    system("pause");// dừng chương trình và xem kết quả trên màn hình console 
+    return 0;//do hàm main có kiểu trả về là int 
+}
